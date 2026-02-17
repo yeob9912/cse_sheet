692354b433cd7e44cad4d990012d89ed53b34001
yeob9912 <obssiyehwala@gmail.com>
2026-02-17 21:29:10 +0300

    Implement string case conversion based on letter counts
    Add functionality to count uppercase and lowercase letters in a string and convert the string based on the counts.

---
+++ A_Word.py
@@ -0,0 +1,17 @@
+
+s=input()
+count_upper=0
+count_lower=0
+for i in s:
+    if i.isupper():
+        count_upper+=1
+    else:
+        count_lower+=1
+if count_lower==count_upper or count_upper < count_lower:
+    s=s.lower()
+    print(s)
+else:
+    s=s.upper()
+    print(s)
+    
+ 
